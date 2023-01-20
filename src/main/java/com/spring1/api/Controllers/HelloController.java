59a35ede3f44a87073ddbd2424a92db2aef642fc
Rafael Longo <rafaelongo45@gmail.com>
2023-01-20 09:25:31 -0300

    Creates controllers folder

--- src/main/java/com/spring1/api/Controllers/HelloController.java
+++ src/main/java/com/spring1/api/Controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.spring1.api;
+package com.spring1.api.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,6 @@ public class HelloController {
   
     @GetMapping
     public String HelloSpring(){
-      return "First route using spring";
+      return "Hello Spring";
     }
 }
